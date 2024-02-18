5a824727fd85dc4449cec1932e9493fda88cc3de
asukim <asukim2020@gmail.com>
2024-02-18 20:17:41 +0900

    Initial Commit

---
+++ AnimalKR/AnimalKRApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalKRApp.swift
+//  AnimalKR
+//
+//  Created by 김현구 on 2/18/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimalKRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
